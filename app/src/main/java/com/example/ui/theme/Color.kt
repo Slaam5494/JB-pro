6b4a3ed441f15ba11c13d21a29a3c7995902cd69
Slaam5494 <mi6757108@gmail.com>
2026-08-25 19:14:33 +0500

    Update Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -2,31 +2,16 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Lumina 9D Cyberpunk Palette
-val CyanPrimary = Color(0xFF00F2FE)
-val CyanSecondary = Color(0xFF0099FF)
-val CyanGlow = Color(0x6600F2FE)
+val CyanPrimary = Color(0xFF00E5FF)
+val CyanSecondary = Color(0xFF18FFFF)
+val PurplePrimary = Color(0xFF7C4DFF)
+val EmeraldAccent = Color(0xFF00E676)
 
-val PurplePrimary = Color(0xFFC05CFF)
-val PurpleSecondary = Color(0xFF7B1FA2)
-val PurpleGlow = Color(0x66C05CFF)
-
-val EmeraldAccent = Color(0xFF00FFAA)
-val EmeraldGlow = Color(0x6600FFAA)
-
-val WarningGold = Color(0xFFFFB300)
-val DangerRed = Color(0xFFFF3333)
-
-val VoidDark = Color(0xFF010308)
-val DarkSurface = Color(0xFF050C1A)
-val DarkCardSurface = Color(0xFF0A1932)
-val DarkCardSecondary = Color(0xFF0E2242)
-
-val CardBorderCyan = Color(0x6600F2FE)
-val CardBorderPurple = Color(0x66C05CFF)
-val CardBorderEmerald = Color(0x6600FFAA)
+val VoidDark = Color(0xFF0A0E17)
+val DarkBackground = Color(0xFF0F141C)
+val DarkSurface = Color(0xFF161C26)
+val DarkCardSurface = Color(0xFF1C2431)
+val DarkCardSecondary = Color(0xFF232D3F)
 
 val TextPrimary = Color(0xFFFFFFFF)
-val TextSecondary = Color(0xFF8B949E)
-val TextCyan = Color(0xFF00F2FE)
-val TextPurple = Color(0xFFC05CFF)
+val TextSecondary = Color(0xFF9E9E9E)
